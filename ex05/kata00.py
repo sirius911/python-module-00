3980d8a4e0fb4219777bbfe33db088548559b802
sirius911 <cyrille.lorin@yahoo.fr>
2022-07-09 20:29:03 +0200

    ADD:ex05

---
+++ ex05/kata00.py
@@ -0,0 +1,3 @@
+t = (19, 42, 21)
+string = f"The {len(t)} numbers are: {t[0]}, {t[1]}, {t[2]}"
+print (string)
---
+++ ex05/kata01.py
@@ -0,0 +1,8 @@
+languages = {
+'Python': 'Guido van Rossum',
+'Ruby': 'Yukihiro Matsumoto',
+'PHP': 'Rasmus Lerdorf',
+}
+
+for langue, user in languages.items():
+    print(f"{langue} was creatde by {user}")
